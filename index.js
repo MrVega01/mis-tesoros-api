43fae022d2dbb85fc076d99a2ca2eef8ac31e45b
MrVega01 <ignaciovega2003@outlook.com>
2024-06-01 11:18:08 -0400

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { createConnection } from './database.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 // GET
 app.get('/products', async (req, res, next) => {
